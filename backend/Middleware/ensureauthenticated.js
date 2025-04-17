23c270832ed2025bdf7f0fe9e99164ee1ddee813
my-suraj5656 <161513600+my-suraj5656@users.noreply.github.com>
2025-04-17 15:15:31 +0530

    Update ensureauthenticated.js

--- backend/Middleware/ensureauthenticated.js
+++ backend/Middleware/ensureauthenticated.js
@@ -19,3 +19,39 @@ const ensureAuthenticated = (req, res, next) => {
 };
 
 module.exports = ensureAuthenticated;
+const arr = ["The quick brown fox jumped over the lazy dog", "o"];
+
+function smallestword(arr) {
+    const letter = arr.splice(arr.length - 1)
+    // console.log(letter);
+
+    let result = []
+    arr.forEach((item) => {
+        let data = item.split(" ")
+        // console.log(data);
+
+        let allstr = []
+        data.forEach((item) => {
+            // console.log(typeof item);
+            if (item.includes(letter)) {
+                allstr.push(item)
+
+            }
+        })
+        // console.log(allstr);
+
+        let shortword = allstr[0]
+
+        for (let i = 1; i < allstr.length; i++) {
+            if (allstr[i].length <= shortword.length) {
+                shortword = allstr[i]
+                result.push(shortword)
+                break
+            }
+        }
+    })
+    return result.join("")
+}
+
+let finalres = smallestword(arr)
+console.log(finalres);
